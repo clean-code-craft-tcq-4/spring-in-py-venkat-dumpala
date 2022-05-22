60e919d5a7bc0e4cad7b9d304775dc871c2e691c
venkat-dumpala <105343053+venkat-dumpala@users.noreply.github.com>
2022-05-22 22:15:06 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,19 +1,7 @@
+import math
+
 def calculateStats(numbers):
-  len1 = len(numbers);
-  
-    
-
-   total = sum(map(float,numbers));
-   avg = total/len1;
-    
-
-  
- 
-  
-
-  
-  max_num = max(numbers);
-  print("max value :", max_num);
-  min_num = min(numbers);
-  print("min value :", min_num);
-  return avg, max_num, min_num
+  if(len(numbers) == 0):
+    return {"avg" : math.nan, "max" : math.nan, "min" : math.nan}
+  else:
+    return{"avg" : sum(numbers)/len(numbers), "max" : max(numbers), "min" : min(numbers)}
