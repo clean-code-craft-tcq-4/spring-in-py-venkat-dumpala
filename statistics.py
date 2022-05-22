5835966f7ab13ee81d666b42847151c04d33d32a
venkat-dumpala <105343053+venkat-dumpala@users.noreply.github.com>
2022-05-22 21:37:34 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,22 @@
-
 def calculateStats(numbers):
-  return None
+  len = len(numbers);
+  
+    
+  while(1):
+    if (list(numbers) or len(numbers)) != 0:
+      total = sum(map(float,numbers));
+      avg = total/len;
+    
+    else:
+      continue
+  
+  
+ 
+  
+
+  
+  max_num = max(numbers);
+  print("max value :", max_num);
+  min_num = min(numbers);
+  print("min value :", min_num);
+  return avg, max_num, min_num
